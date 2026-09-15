bd763717877221e0ffa21648b141f92f6399b961
VArantes301 <dearantesv@gmail.com>
2026-09-15 20:34:03 -0300

    feat:: Image upload update complete

--- server/src/services/diary.js
+++ server/src/services/diary.js
@@ -45,7 +45,6 @@ class diaryService {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0');
     const day = String(today.getDate()).padStart(2, '0');
